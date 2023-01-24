53cfd54bed2fb75874b92ef554e2decc6c827535
Deepak Rawat <awsmyo1234@gmail.com>
2023-01-24 08:12:11 +0530

    library

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-
+import  postcss  from "rollup-plugin-postcss";
 
 export default [
   {
@@ -23,6 +23,7 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
