9db6300a78913e729994b39357e5177f4c37bf5c
Deepak Rawat <awsmyo1234@gmail.com>
2023-01-25 09:23:53 +0530

    bugfix

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,17 +8,15 @@ interface ButtonProps {
     "border-radius"?: string
 }
 
-const Button = (props: ButtonProps) =>{
-    let propValues: Array<{}> = Object.keys(props).map((propKey: string) =>{
-        if(propKey === "border-radius" && props["border-radius"]) {
+const Button= (props: ButtonProps) =>{
+    let propValues= Object.keys(props).map((propKey: string) =>{
+        if(propKey === "border-radius" && props["border-radius" as keyof typeof props]==="true") {
             return "border-radius"
         } else if(propKey!=="label"){
             return props[propKey as keyof typeof props]
-        } 
-        else{
+        } else{
             return ""
         }
-        return ""
     });
 
     return <button className={propValues.join(" ")}>{props.label}</button>
